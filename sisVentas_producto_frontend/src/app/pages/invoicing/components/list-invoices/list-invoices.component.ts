a4175e0d7da7d37c45ef379872854acb4aebb4c6
Ivancontry <ivancontry.13@gmail.com>
2022-06-06 23:17:40 -0500

    cambiando color y agregando anulacion

--- sisVentas_producto_frontend/src/app/pages/invoicing/components/list-invoices/list-invoices.component.ts
+++ sisVentas_producto_frontend/src/app/pages/invoicing/components/list-invoices/list-invoices.component.ts
@@ -1,35 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { InvoiceMaster } from '../../models/invoice-master';
-import { CreateInvoiceService } from '../../services/create-invoice.service';
-import { ListInvoicesService } from '../../services/list-invoices.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {InvoiceMaster} from '../../models/invoice-master';
+import {CreateInvoiceService} from '../../services/create-invoice.service';
+import {ListInvoicesService} from '../../services/list-invoices.service';
 
 @Component({
-  selector: 'app-list-invoices',
-  templateUrl: './list-invoices.component.html',
-  styleUrls: ['./list-invoices.component.scss']
+    selector: 'app-list-invoices',
+    templateUrl: './list-invoices.component.html',
+    styleUrls: ['./list-invoices.component.scss']
 })
-export class ListInvoicesComponent implements OnInit{
+export class ListInvoicesComponent implements OnInit {
 
     invoices: InvoiceMaster[] = [];
 
-    constructor(private _listInvoicesService: ListInvoicesService,private router: Router) {}
+    constructor(private _listInvoicesService: ListInvoicesService, private router: Router) {
+    }
 
     ngOnInit(): void {
         this.getInvoices();
     }
 
     getInvoices() {
-        this._listInvoicesService.getInvoices().subscribe(t=>
-            {
-                this.invoices = Object.assign([],t.invoiceMasters);
-            });
+        this._listInvoicesService.getInvoices().subscribe(t => {
+            this.invoices = Object.assign([], t.invoiceMasters);
+        });
     }
 
-    CreateInvoice(){
+    CreateInvoice() {
         this.router.navigate(['invoicing/create'])
     }
-    seeDetail(invoice: InvoiceMaster):void{
+
+    seeDetail(invoice: InvoiceMaster): void {
         this.router.navigate([`invoicing/${invoice.id}/detail`])
+    }
+
+    cancelInvoice(invoice: InvoiceMaster) {
+        this._listInvoicesService.cancelInvoice(invoice.id, new Date()).subscribe(result => {
+            if (!result) return;
+            alert(result.message);
+            this.getInvoices();
+        });
     }
 }
--- sisVentas_producto_frontend/src/app/pages/invoicing/services/list-invoices.service.ts
+++ sisVentas_producto_frontend/src/app/pages/invoicing/services/list-invoices.service.ts
@@ -1,16 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ListInvoicesService {
 
-  constructor(private _httpClient: HttpClient) { }
+    constructor(private _httpClient: HttpClient) {
+    }
 
-  getInvoices(): Observable<any>{
-    return this._httpClient.get<any>(`${environment.baseUrl}invoice`);
-}
+    getInvoices(): Observable<any> {
+        return this._httpClient.get<any>(`${environment.baseUrl}invoice`);
+    }
+
+    cancelInvoice(id: number | undefined, dateCancel: Date): Observable<{ message: string }> {
+        return this._httpClient.put<{ message: string }>(`${environment.baseUrl}invoice`, {
+            id: id,
+            dateCancel: dateCancel
+        });
+    }
 }
